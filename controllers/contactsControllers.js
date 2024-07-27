137ead47aac2bc83bba08a0f8d6da75d328126c9
Maksym-Zyza <mmziza88@gmail.com>
2024-07-27 14:07:33 +0300

    added contactsServices, contacts.json

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,6 +1,9 @@
-import contactsService from "../services/contactsServices.js";
+import { listContacts } from "../services/contactsServices.js";
 
-export const getAllContacts = (req, res) => {};
+export const getAllContacts = async (req, res) => {
+  const result = await listContacts();
+  res.json(result);
+};
 
 export const getOneContact = (req, res) => {};
 
